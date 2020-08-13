483db9ae88fc44fbff0e0d86e5422d50906917d0
idonia <sidoniag@gmail.com>
2020-08-13 14:43:39 -0500

    continue set up

--- models/User.js
+++ models/User.js
@@ -7,11 +7,45 @@ class User extends Model {}
 // define table columns and configuration
 User.init(
   {
-    // TABLE COLUMN DEFINITIONS GO HERE
+    // define an id column
+    id:{
+      // use the special Sequelize DataTypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+      // instruct that this is the Primary Key
+      primaryKey: true,
+      // turn on auto increment
+      autoIncrement: true
+    },
+    // define a username column
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // define an email column
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // there cannot be any duplicate email values in this table
+      unique: true,
+      // if allowNul is set to false, we can run our data through validators before creating the table data
+      validate: {
+        isEmail: true
+      }
+    },
+    // define a password column
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // this means the password must be at least four characters long
+        len: [4]
+      }
+    }
   },
   {
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
