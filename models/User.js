b43b02032844a8d53f9c1805203c1111c717d5f9
idonia <sidoniag@gmail.com>
2020-08-14 15:21:59 -0500

    add hook

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -45,6 +46,14 @@ User.init(
     }
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      beforeCreate(userData) {
+        return bcrypt.hash(userData.password, 10).then(newUserData => {
+          return newUserData
+        });
+      }
+    },
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
