814a6ac6ab7459883a729e5635b0b9fc9f7a7ec2
idonia <sidoniag@gmail.com>
2020-08-16 17:36:10 -0500

    pw fix

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our databse, pass in your MYSQL information for username and password
-const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
+const sequelize = new Sequelize('just_tech_news_db', 'root', 'RaFb1308&$', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
