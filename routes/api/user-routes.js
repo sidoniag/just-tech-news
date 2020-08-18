7360fea9d0bf60047a40c555ab9ad969f0012e84
idonia <sidoniag@gmail.com>
2020-08-18 17:13:55 -0500

    comment api endpoints

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -24,12 +24,15 @@ router.get('/:id', (req, res) => {
         include: [
             {
                 model: Post,
-                attributes: [
-                    'id',
-                    'title',
-                    'post_url',
-                    'created_at'
-                ]
+                attributes: ['id','title','post_url','created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'create_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
             },
             {
                 model: Post,
