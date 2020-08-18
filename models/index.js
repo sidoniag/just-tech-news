c75800eacd0a60ee4f274fef080e2129fe9ef54d
idonia <sidoniag@gmail.com>
2020-08-17 21:55:09 -0500

    need to debug hasmany

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+// user can make many post
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+// a post can only belong to one user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
