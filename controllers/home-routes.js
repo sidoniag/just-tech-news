0c152645a077273ca577c9d41afa7a020bb9eae7
idonia <sidoniag@gmail.com>
2020-08-19 17:53:04 -0500

    add login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
-const posts = dbPostData.map(post => post.get({ plain: true }));
 
 router.get('/', (req, res) => {
     Post.findAll({
@@ -29,12 +28,17 @@ router.get('/', (req, res) => {
     })
     .then(dbPostData => {
         // pass a single post object into the homepage template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
         res.render('homepage', { posts });
     })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
-  });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
 
 module.exports = router;
