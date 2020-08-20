a07475aec3b20a618c99f1ec3ae3bad2aa375a09
idonia <sidoniag@gmail.com>
2020-08-19 19:12:13 -0500

    need to debug login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,10 +63,14 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password,
     })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+      
+          res.json(dbUserData);
+        });
     });
 });
 
@@ -90,7 +94,14 @@ router.post('/login', (req, res) => {
             return;
         }
 
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
         res.json({ dbUserData, message: 'You are now logged in!'});
+        });
     });
 });
 
@@ -137,6 +148,17 @@ router.delete('/:id', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+          res.status(204).end();
+        });
+      }
+      else {
+        res.status(404).end();
+      }
 });
 
 module.exports = router;
