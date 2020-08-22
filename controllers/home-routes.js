8586659ca2c6e87c7159369eb1585924df976991
idonia <sidoniag@gmail.com>
2020-08-22 18:42:03 -0500

    login debug needed

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -38,7 +38,7 @@ router.get('/', (req, res) => {
         });
 });
 
-router.get('/login', (req, res) => {
+router.get('/login', (req,res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
     return;
